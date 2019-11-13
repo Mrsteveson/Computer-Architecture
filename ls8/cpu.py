07c576564abec7015280b703bd65e8a65853d005
mrsteveson <patrick.steveson727@gmail.com>
2019-11-12 18:02:02 -0600

    wednesday mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -22,22 +22,17 @@ class CPU:
     def load(self):
         """Load a program into memory."""
         address = 0
-
-        # For now, we've just hardcoded a program:
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        try:
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    if line[0].startswith('0') or line[0].startswith('1'):
+                        split = line.split('#')
+                        number = split[0].strip()
+                        self.ram[address] = int(number, 2)
+                        address += 1
+        except FileNotFoundError:
+            print("Attempted to load an invalid file.")
+            sys.exit(1)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -45,6 +40,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        if op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -74,6 +71,7 @@ class CPU:
         HLT = 0b00000001
         LDI = 0b10000010
         PRN = 0b01000111
+        MUL = 0b10100010
 
         running = True
         while running is True:
@@ -89,6 +87,9 @@ class CPU:
                 operand_a = self.ram[self.pc + 1]
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
 
 
 
